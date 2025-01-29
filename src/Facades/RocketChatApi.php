ba218836789395a92977abb7d8f44bca16362ff5
Erdin Hermawan <erdinhermawann@gmail.com>
2025-01-29 22:16:18 +0700

    Added methods and types to RocketChatApi facade

--- src/Facades/RocketChatApi.php
+++ src/Facades/RocketChatApi.php
@@ -4,9 +4,24 @@ namespace ErdinHrmwn\RocketChat\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static RocketChatService users()
+ * @method static RocketChatService roles()
+ * @method static RocketChatService permissions()
+ * @method static RocketChatService groups()
+ * @method static RocketChatService channels()
+ * @method static RocketChatService chats()
+ * @method static array getServerInfo()
+ * @method static array me()
+ *
+ * @see \ErdinHrmwn\RocketChat\Http\RocketChatService
+ */
 class RocketChatApi extends Facade
 {
-    protected static function getFacadeAccessor()
+    /**
+     * Get the registered name of the component.
+     */
+    protected static function getFacadeAccessor(): string
     {
         return 'rocket-chat-api';
     }
